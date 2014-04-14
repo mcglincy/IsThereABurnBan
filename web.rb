47cb6daef584f9777d995a474ac41b1664ba0d23
Matt McGlincy <matt.mcglincy@thirteen23.com>
2014-04-14 12:27:47 -0500

    Add some static files.

--- web.rb
+++ web.rb
@@ -1,5 +1,11 @@
 require 'sinatra'
+require 'sinatra/base'
+
+class MyApp < Sinatra::Base
+  set :static, true
+  set :public, File.dirname(__FILE__) + '/static'
+end
 
 get '/' do
-  "Hello World!"
+  File.read(File.join('static', 'index.html'))
 end
